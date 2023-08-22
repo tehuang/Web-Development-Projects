358b5050b562ed1d3dac0ac94573c25b9e9add33
Teresa Huang <teresayhhuang@gmail.com>
2023-08-22 16:28:23 -0400

    Level 1: Plain text
    - Created a database to save the data
    - Created a username/password system where the password is saved in plain text

---
+++ 37.0 Authentication and Security/Secrets - Starting Code/app.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose  from "mongoose";
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+//Create a local db
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+//Define user schema
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");    
+});
+
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");    
+});
+
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");    
+});
+
+//Where user register their account
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email:req.body.username,
+        password: req.body.password
+    });
+    newUser.save().then(function(){
+        res.render("secrets.ejs");  //We render the secret page unless the user has registered their account
+    }).catch(function(err){
+        console.log(err);
+    });
+});
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.findOne({email:username}).then(function(foundUser){
+        if(foundUser.password===password){
+            res.render("secrets.ejs");
+        }
+    }).catch(function(err){
+        console.log(err);
+    }); 
+    
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
